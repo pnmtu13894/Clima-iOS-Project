23b0076d1f21242560b4cda7b84d0249d4990b2c
minhtu <pnmtu13894@gmail.com>
2018-09-27 01:06:25 +0700

    Update-UI

---
+++ Clima/CollectionWeatherCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionWeatherCellCollectionViewCell.swift
+//  Clima
+//
+//  Created by Minh Tú on 9/27/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CollectionWeatherCellCollectionViewCell: UICollectionViewCell {
+    
+}
